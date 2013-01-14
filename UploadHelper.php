cf13499ee2822f48db4cb6452c4c428e33a03b3d
Rafael Pinheiro <rafaelwendel@hotmail.com>
2013-01-14 21:46:11 -0200

    Include the vars and methods comments

--- UploadHelper.php
+++ UploadHelper.php
@@ -4,18 +4,82 @@
  *
  * @author Rafael Wendel Pinheiro
  * @version 1.0
+ * @link https://github.com/rafaelwendel/UploadHelper
  */
 class Upload_Helper {
     
+    /**
+     * The file to be sent
+     * @access protected
+     * @var array
+     */
     protected $_file;
+    
+    
+    /**
+     * The extension of file
+     * @access protected
+     * @var String
+     */
     protected $_ext;
+    
+    
+    /**
+     * The folder to receive the file
+     * @access protected
+     * @var String
+     */
     protected $_uploads_folder;
+    
+    
+    /**
+     * The uploaded file path
+     * @access protected
+     * @var String
+     */
     protected $_file_path;
+    
+    
+    /**
+     * The new name of file
+     * @access protected
+     * @var String
+     */
     protected $_file_name = null;
+    
+    
+    /**
+     * The allowed extensions in the upload
+     * @access protected
+     * @var array
+     */
     protected $_allowed_exts = array();
+    
+    
+    /**
+     * Overwrite file with same name?
+     * @access protected
+     * @var Boolean
+     */
     protected $_overwrite = true;
+    
+    
+    /**
+     * The error message
+     * @access protected
+     * @var String
+     */
     protected $_error;
     
+    
+    /**
+     * Constructor method. You can define the file, uploads folder and new file name
+     * @access public
+     * @param $file File The file to be upload
+     * @param $uploads_folder String The path of the receive folder
+     * @param $file_name String The new name
+     * @return Void
+     */
     public function __construct($file = '', $uploads_folder = '', $file_name = '') {
         if (isset ($file)){
             $this->set_file($file);
@@ -28,15 +92,36 @@ class Upload_Helper {
         }
     }
     
+    
+    /**
+     * Set a file
+     * @access public
+     * @param $file File The file to be upload
+     * @return Void
+     */
     public function set_file($file){
         $this->_file = $file;
         $this->set_ext($file);
     }
     
+    
+    /**
+     * Set the extension of file
+     * @access protected
+     * @param $file File The file will be sent
+     * @return Void
+     */
     protected function set_ext($file){
         $this->_ext = strtolower(end(explode('.', $file['name'])));
     }
     
+    
+    /**
+     * Set the folder to receive the file
+     * @access public
+     * @param $uploads_folder String The path of the receive folder
+     * @return Void
+     */
     public function set_uploads_folder($uploads_folder){
         if (substr ($uploads_folder, -1) == '/'){
             $this->_uploads_folder = $uploads_folder;
@@ -46,10 +131,24 @@ class Upload_Helper {
         }
     }
     
+    
+    /**
+     * Set the new name of the file
+     * @access public
+     * @param $file_name String The new name
+     * @return Void
+     */
     public function set_file_name($file_name){
         $this->_file_name = $file_name;
     }
     
+    
+    /**
+     * Set the allowed extensions in the upload
+     * @access public
+     * @param $file_name String The new name
+     * @return Void
+     */
     public function set_allowed_exts($allowed_exts){
         if (is_array ($allowed_exts)){
             $this->_allowed_exts = $allowed_exts;
@@ -59,22 +158,54 @@ class Upload_Helper {
         }
     }
     
+    
+    /**
+     * Overwrite file with same name? (true or false)
+     * @access public
+     * @param $param Boolean Yes(true) or no(false)
+     * @return Void
+     */
     public function set_overwrite($param){
         $this->_overwrite = (bool) $param;
     }
     
+    
+    /**
+     * Set a error message
+     * @access protected
+     * @param $error String The error message
+     * @return Void
+     */
     protected function set_error($error){
         $this->_error = $error;
     }
     
+    
+    /**
+     * Get the error message
+     * @access public
+     * @return String The error message
+     */
     public function get_error(){
         return $this->_error;
     }
     
+    
+    /**
+     * Get the uploaded file path
+     * @access public
+     * @return String The uploaded file path
+     */
     public function get_file_path(){
         return $this->_file_path;
     }    
     
+    
+    /**
+     * Keep the file with the same name
+     * @access protected
+     * @return Void
+     */
     protected function some_name(){
         $tmp_name = explode('.', $this->_file['name']);        
         unset($tmp_name[count($tmp_name) - 1]);
@@ -82,6 +213,12 @@ class Upload_Helper {
         $this->_file_name = implode('.', $tmp_name);
     }
     
+    
+    /**
+     * Checks whether to overwrite files with the same name. If not, creates name incremented ($name, $name_1, $name_2, $name_n)
+     * @access protected
+     * @return Void
+     */
     protected function verify_overwrite(){
         if (!$this->_overwrite){
             $tmp_name = $this->_file_name;
@@ -94,6 +231,12 @@ class Upload_Helper {
         }
     }
     
+    
+    /**
+     * Validates requirements for uploading
+     * @access protected
+     * @return Boolean True if valid
+     */
     protected function is_valid(){
         if (empty ($this->_file['name'])){
             $this->set_error('File is not set');
@@ -110,6 +253,12 @@ class Upload_Helper {
         return true;
     }
     
+    
+    /**
+     * Upload the file
+     * @access public
+     * @return Boolean True if file has been uploaded
+     */
     public function upload_file(){
         if (!$this->is_valid()){
             return false;
